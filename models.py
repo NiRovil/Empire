36e36e587264d51aded05155a18adab27509ed9f
NiRovil <nicolas199851@hotmail.com>
2022-09-01 18:21:47 -0300

    Definindo a função de saída de estoque.

--- models.py
+++ models.py
@@ -3,8 +3,8 @@ import psycopg2
 class Usuario:
 
     def __init__(self, nome, senha):
-        self.nome = nome 
-        self.senha = senha
+        self._nome = nome 
+        self._senha = senha
 
     def cadastro(self):
 
@@ -15,7 +15,7 @@ class Usuario:
             password='nicolasvx123'
         )
 
-        print(self.nome)
+        print(self._nome)
 
         cur = con.cursor()
         cur.execute("CREATE TABLE IF NOT EXISTS public.usuario (id serial PRIMARY KEY, nome varchar(50), senha varchar(50));")
@@ -24,13 +24,13 @@ class Usuario:
 
         cadastrado = False
         for user in users:
-            if self.nome == user[1]:
+            if self._nome == user[1]:
                 cadastrado = True
             
         if cadastrado:
             return print('Usuário já existe!')
 
-        cur.execute("INSERT INTO public.usuario (nome,senha) VALUES (%s, %s)", (self.nome, self.senha))
+        cur.execute("INSERT INTO public.usuario (nome,senha) VALUES (%s, %s)", (self._nome, self._senha))
         con.commit()        
         cur.close()
         con.close()
@@ -50,7 +50,7 @@ class Usuario:
         cur.execute("SELECT * FROM usuario;")
         users = cur.fetchall()
         for user in users:
-            if self.nome == user[1] and self.senha == user[2]:
+            if self._nome == user[1] and self._senha == user[2]:
                 cur.close()
                 con.close()
                 return True
@@ -137,11 +137,45 @@ class Estoque(Usuario):
                     cadastrado = True
 
             if cadastrado:
-                pass
+                resposta = input('Deseja deletar o item da base de dados? [s/n] ')
+                if resposta == 's':
 
-            a = input('Produto não encontrado no banco de dados, deseja cadastrar? [s/n]')
-            if a == 's':
+                    con = psycopg2.connect(
+                        host='localhost',
+                        dbname='empire',
+                        user='postgres',
+                        password='nicolasvx123'
+                    )
+
+                    cur = con.cursor()
+                    cur.execute("DELETE FROM public.estoque WHERE nome_produto=%s", (nome_produto,))
+                    con.commit()
+                    cur.close()
+                    con.close()
+
+                    print('Produto deletado com sucesso!')
+                    
+                    return Estoque.entrada_estoque(self)
+
+                elif resposta == 'n':
+                    
+                    print('Operação cancelada. O que deseja fazer a seguir?\n')
+                    print('[1] - Entrada de estoque.')
+                    print('[2] - Sair.')
+                    resposta = input('--> ')
+
+                    if resposta == '1':
+                        return Estoque.entrada_estoque(self)
+                    elif resposta == '2':
+                        print('Obrigado por usar o sistema!')
+                        return exit()
+                    
+            resposta = input('Produto não encontrado no banco de dados, deseja cadastrar? [s/n] ')
+            if resposta == 's':
                 return Estoque.entrada_estoque(self)
+            elif resposta == 'n':
+                print('Obrigado por usar o sistema!')
+                return exit()
 
         return print('Verifique o usuário e a senha. Ou cadastre-se!')
     

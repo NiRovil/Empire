0fde369a0447abccc07b5aaa5bf15dd52fa9fc9d
NiRovil <nicolas199851@hotmail.com>
2022-10-31 19:14:51 -0300

    Aprimorando o carrinho de compras.

--- vendas.py
+++ vendas.py
@@ -11,6 +11,7 @@ class Vendas(Usuario):
         
     def venda(self):
 
+        # Conecta ao banco de dados.
         con = psycopg2.connect(
             host='localhost',
             dbname='empire',
@@ -23,25 +24,32 @@ class Vendas(Usuario):
         cur.execute("SELECT * FROM public.estoque;")
         produtos = cur.fetchall()
 
-        respostas = []
+        respostas = {}
 
+        # Se houver produto no estoque inicia o módulo vendas.
         if produtos:
 
-            layout_pesquisa = [[sg.Text('Quais produtos deseja adicionar ao carrinho?')],]
+            layout_pesquisa = [[sg.Text('Quais produtos deseja adicionar ao carrinho?')]]
             
+            # Define o layout da aba de vendas.
+            # O for abaixo, lista todos os produtos disponíveis no banco de dados.
             for produto in produtos:
-                layout_pesquisa.append([sg.Button(f'{produto[0]} - {produto[1]}', key=f'{produto[1]}')])
-            
-            layout_pesquisa.append([[sg.Button('Cesta', key='cart'), sg.Button('Finalizar', key='end')], [sg.Button('Menu', key=''), sg.Button('Sair', key='')]])
-            
-            window_pesquisa = sg.Window('Empire / TESTE', layout=layout_pesquisa)
+                layout_pesquisa.append(
+                    [sg.Text(f'{produto[0]} - {produto[1]}'), sg.Text(f'quantidade: {produto[2]}'), sg.Button('Adicionar', key=f'{produto[1]}')]
+                )
+        
+            layout_pesquisa.append(
+                [[sg.Button('Cesta', key='cart'), sg.Button('Finalizar', key='end')], 
+                [sg.Button('Menu', key=''), sg.Button('Sair', key='')]]
+            )
+           
+            window_pesquisa = sg.Window('Empire / Venda', layout=layout_pesquisa)
 
+            # Enquanto a janela estiver aberta as ações feitas pelo usuário serão identificadas.
             while True:
                 event_pesquisa, values_pesquisa = window_pesquisa.read()
 
-                print(event_pesquisa)
-                print(values_pesquisa)
-
+                # Se o produto estiver nos itens previamente adicionados uma janela de aviso é mostrada.
                 if respostas and event_pesquisa in respostas:
                     layout_erro = [
                         [sg.Text('Produto já adicionado, selecione outro ou finalize a operação!')],
@@ -55,22 +63,42 @@ class Vendas(Usuario):
                     if event_erro == 'back':
                         window_erro.close()
                 
+                # Mostra o atual estado do carrinho de compras.
                 elif event_pesquisa == 'cart':
                     if respostas and str(event_pesquisa) == 'cart':
                         print(respostas)
-                        layout_cart = []
+                        layout_cart = [[sg.Text('Produtos adicionados:')]]
                         for item in respostas:
                             print(item)
-                            layout_cart.append([sg.Text(f'{produto}')])
-                        sg.Window('Empire / Carrinho', layout=layout_cart).read()
+                            layout_cart.append([sg.Text(f'{item}')])
+                        window = sg.Window('Empire / Carrinho', layout=layout_cart)
+                        event, values = window.read()
 
-                    return False
-                               
+                        if event == sg.WIN_CLOSED:
+                            window.close()
+
+                    # Se não houver nenhum item no carrinho, um aviso é mostrado.
+                    else:
+                        layout_cart = [[sg.Text('Nenhum produto selecionado!')]]
+                        window = sg.Window('Empire / Carrinho', layout=layout_cart)
+                        event, values = window.read()
+
+                        if event == sg.WIN_CLOSED:
+                            window.close()
+
+                # Adiciona um item ao carrinho.          
                 else:
                     for produto in produtos:
                         if event_pesquisa == produto[1]:
-                            respostas.append(event_pesquisa)       
+                            layout = [
+                                [sg.Text('Selecione a quantidade: '), sg.InputText(key='q')]
+                            ]
 
+                            sg.Window('Empire / Quantidade', layout=layout).read()
+                            respostas[event_pesquisa] = produto[2]
+                            print(respostas)      
+
+        # Se não houver produto, uma mensagem é exibida.
         else:
             print('Nenhum produto no estoque!')
             print('O que deseja fazer a seguir?\n')
